38134ce809c3bd3c07a6e85bd3e77f2f928546f6
torrapipes <crbergas@gmail.com>
2019-03-12 12:49:15 +0100

    Método de comparación añadido

--- src/main/java/ejercicios/Employee.java
+++ src/main/java/ejercicios/Employee.java
@@ -18,4 +18,27 @@ public class Employee {
         return name;
 
     }
+
+    public boolean equals(Object obj) {
+
+        boolean isEqual = false;
+
+        // We compare objects of the Employee class with the objects of
+        // Employee class or its descendants
+
+        if (obj instanceof Employee) {
+
+            // If two have the same name, consider them equal.
+
+            Employee e = (Employee) obj;
+
+            String n = e.getName();
+
+            isEqual = n.equals(this.name);
+
+        }
+
+        return isEqual;
+
+    }
 }
