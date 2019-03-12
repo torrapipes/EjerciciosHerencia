8ac4d4bd1278628619b1e5ee79e91e8ccca3d5fd
torrapipes <crbergas@gmail.com>
2019-03-12 13:54:37 +0100

    Creadas las clases AOSub que hereda de AOSuper

---
+++ src/main/java/ejercicios/AOSub.java
@@ -0,0 +1,27 @@
+package ejercicios;
+
+public class AOSub extends AOSuper{
+
+
+    public void print() {
+
+        // Call print() method of AOSuper class
+
+        super.print();
+
+        // Print a message
+
+        System.out.println("Inside AOSub.print()");
+
+    }
+
+
+    public void callOverridenPrint() {
+
+
+        // Call print() method of AOSuper class
+
+        super.print();
+
+    }
+}
---
+++ src/main/java/ejercicios/AOSuper.java
@@ -0,0 +1,14 @@
+package ejercicios;
+
+public class AOSuper {
+
+
+    public AOSuper(){}
+
+    public void print(){
+
+        System.out.println("Inside AOSuper.print()");
+
+    }
+
+}
