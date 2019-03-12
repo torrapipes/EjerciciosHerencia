d5d1ef1991021ace07d3a2741a6c05608a7a92ab
torrapipes <crbergas@gmail.com>
2019-03-12 12:58:29 +0100

    Clase EarlyBindingTest y método y variables de EarlyBindingSuper creadas.

---
+++ src/main/java/ejercicios/EarlyBindingSub.java
@@ -0,0 +1,9 @@
+package ejercicios;
+
+public class EarlyBindingSub {
+
+
+    public EarlyBindingSub(){}
+
+
+}
---
+++ src/main/java/ejercicios/EarlyBindingSuper.java
@@ -0,0 +1,22 @@
+package ejercicios;
+
+public class EarlyBindingSuper {
+
+
+    public String str = "EarlyBindingSuper";
+
+
+    public int count = 100;
+
+
+    public EarlyBindingSuper(){}
+
+
+    public void print(){
+
+        System.out.println("Inside EarlyBindingSuper.print()");
+
+    }
+
+
+}
