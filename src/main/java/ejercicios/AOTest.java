8b9e0d8048a7d2331894d4da495cb68e4459edb4
torrapipes <crbergas@gmail.com>
2019-03-12 13:57:41 +0100

    Creada la clase AOTest y todo funciona

---
+++ src/main/java/ejercicios/AOTest.java
@@ -0,0 +1,21 @@
+package ejercicios;
+
+public class AOTest {
+
+
+    public static void main(String[] args) {
+
+
+        AOSub aoSub = new AOSub();
+
+
+        aoSub.print();
+
+
+        aoSub.callOverridenPrint();
+
+
+    }
+
+
+}
