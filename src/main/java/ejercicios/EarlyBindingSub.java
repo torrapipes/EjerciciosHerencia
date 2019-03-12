7dfa51633b852a4b690de13e14166c73c14efbe6
torrapipes <crbergas@gmail.com>
2019-03-12 13:01:50 +0100

    Método y variables de EarlyBindingSub creados

--- src/main/java/ejercicios/EarlyBindingSub.java
+++ src/main/java/ejercicios/EarlyBindingSub.java
@@ -3,7 +3,20 @@ package ejercicios;
 public class EarlyBindingSub {
 
 
+    public String str = "EarlyBindingSub";
+
+
+    public int count = 200;
+
+
     public EarlyBindingSub(){}
+
+
+    public void print(){
+
+        System.out.println("Inside EarlyBindingSub.print()");
+
+    }
 
 
 }
