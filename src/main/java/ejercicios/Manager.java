4214e70f205665f1d6f13939d2e2e6ea62df47d5
torrapipes <crbergas@gmail.com>
2019-03-12 12:40:02 +0100

    Clases Manager y UpcaseTest creadas

---
+++ src/main/java/ejercicios/Manager.java
@@ -0,0 +1,24 @@
+package ejercicios;
+
+public class Manager {
+
+    private String name = null;
+
+    public Manager(){
+
+    }
+
+
+    public String getName(){
+
+        return this.name;
+
+    }
+
+
+    public void setName(String name){
+
+        this.name = name;
+
+    }
+}
---
+++ src/main/java/ejercicios/UpcastTest.java
@@ -0,0 +1,21 @@
+package ejercicios;
+
+public class UpcastTest {
+
+    public static void main(String[] args) {
+
+    Employee emp = new Employee();
+
+    emp.setName("Ken Wood");
+
+    Manager mgr = new Manager();
+
+    mgr.setName("Ken Furr"); // Inheritance of setName() at work
+
+    // Print names
+
+     EmpUtil.printName(emp);
+     EmpUtil.printName(mgr); // Upcasting at work
+
+}
+}
